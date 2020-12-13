5c006a4b9f2398b9a6dc166ff6d9a73f66bc09c8
sidodjio <73775316+sidodjio@users.noreply.github.com>
2020-12-13 15:30:28 +0500

    Update worker.py

--- sigumnov/worker.py
+++ sigumnov/worker.py
@@ -1,1 +1,20 @@
-# write your code here
+import socket
+
+s = socket.socket()
+
+s.bind(('0.0.0.0',9333))
+s.listen(1)
+
+conn, addr = s.accept()
+
+print ('connected:',format(addr))
+
+while 1:
+    x = conn.recv(1024)
+    if x:
+        x = int(x.decode())
+        x = str(-x)
+        conn.send(x.encode())
+    else:
+        break
+s.close()
